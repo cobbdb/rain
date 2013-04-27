d42a57a3b9624236ddf4e7230ec5933d5b904bfa
cobbdb <cobbdb@gmail.com>
2013-04-26 23:59:32 -0700

    Map drop down - removed label.

--- index.php
+++ index.php
@@ -1,27 +1,10 @@
 <?php
 
 require "./private/eta.php";
-H::setHome("./private/views/");
+H::setHome("./private/views/", true);
 H::setBase("../base.view");
 
-/*function buildMenuSet($wiki) {
-    $menus = H::render("map.view");
-    $menus .= H::render("graphs.view");
-    $menus .= H::render("results.view");
-    $menus .= H::render("input.view");
-    $menus .= H::render("options.view");
-    $menus .= H::render("help.view", Array(
-        "wiki" => $wiki
-    ));
-    return $menus;
-}*/
-
-
 $head = H::render("head.view");
-
-//$wiki = "http://www.appropedia.org/Rainwater_Collection_Calculator";
-//$menus = buildMenuSet($wiki);
-//$busy = H::render("busy.view");
 $body = H::render("index.view");
 
 echo H::render(null, Array(
