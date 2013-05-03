bbb1d0483e207d507c00a5d7d40dc4d4fcb65d8d
cobbdb <cobbdb@gmail.com>
2013-05-02 22:46:47 -0700

    Refactor on Station class.

--- private/station.php
+++ private/station.php
@@ -2,14 +2,14 @@
 
 /**
  * station.php
- * 
+ *
+ * Stations may contain data points from many years of
+ * rainfall measurements. Not all years have rainfall data
+ * for every month.
+ * TODO: throw exception instead of silent return of error codes.
+ * TODO: Desperately needs static class Month with enums.
  * @author Dan Cobb
  * @since 1.3.0
- * @date September 18, 2012
- */
-
-/**
- * @description lkj
  */
 class Station {
     public $station_id, $lat, $lng, $dist;
@@ -17,12 +17,11 @@ class Station {
     private $sums;
     private $counts;
     
-    
-    function __construct($in_id, $in_lat, $in_lng, $in_dist) {
-        $this->station_id = $in_id;
-        $this->lat = $in_lat;
-        $this->lng = $in_lng;
-        $this->dist = $in_dist;
+    public function __construct($id, $lat, $lng, $dist) {
+        $this->station_id = $id;
+        $this->lat = $lat;
+        $this->lng = $lng;
+        $this->dist = $dist;
         
         for ($i = 0; $i < 12; $i += 1) {
             $this->sums[] = 0;
@@ -30,8 +29,12 @@ class Station {
         }
     }
     
-    // $mo_num is a calendar number -> [1,12]
-    function addRainfall($mo_num, $val) {
+    /**
+     * Add rainfall data point to a month.
+     * @param {Number} no_num Month's calendar number [1,12].
+     * @param {Number} val Rainfall amount in cm.
+     */
+    public function addRainfall($mo_num, $val) {
         // decrement to month index
         $mo_num -= 1;
         
@@ -39,36 +42,40 @@ class Station {
         $this->counts[$mo_num] += 1;
     }
     
-    // $mo_num is a calendar number -> [1,12]
-    function getMonthlyMean($mo_num) {
-        $mo_num -= 1;
+    /**
+     * Calculate mean rainfall in mm for a month.
+     * @param {Number} mo_num Month's calendar number [1,12].
+     * @returns {Number} Mean rainfall in mm.
+     */
+    public function getMonthlyMean($mo_num) {
+        $index = $mo_num - 1;
         
-        if ($this->counts[$mo_num] == 0) {
-            // no data
-            $ave = -99; // error code
-        } else {
-            $ave = $this->sums[$mo_num];
-            $ave /= $this->counts[$mo_num];
-            $ave /= 10; // convert to mm
-            // don't need fractions of a millimetre
-            $ave = round($ave);
+        // Error if no data points for this month.
+        if ($this->counts[$index] == 0) {
+            return -99; // error code
         }
         
-        return $ave;
+        $ave = $this->sums[$index];
+        $ave /= $this->counts[$index];
+        $ave /= 10; // convert to mm
+        // don't need fractions of a millimetre
+        return round($ave);
     }
     
-    function getYearlyMean() {
+    /**
+     * Calculate mean rainfall in mm for a year.
+     * @return {Number} Mean rainfall in mm.
+     */
+    public function getYearlyMean() {
         $ave = 0;
         for ($i=0; $i<12; $i += 1) {
             $m_ave = $this->getMonthlyMean($i + 1);
+            // Error if any month has no data points.
             if ($m_ave == -99) {
-                $ave = -99; // error: incomplete data
-                break;
-            } else {
-                $ave += $m_ave;
+                return -99; // error code
             }
+            $ave += $m_ave;
         }
-        
         return $ave;
     }
 }
