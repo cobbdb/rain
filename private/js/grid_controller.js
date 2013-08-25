3c3b319dd907087134976b3c5be7cf46bf8a2b16
cobbdb <cobbdb@gmail.com>
2013-08-25 13:15:18 -0700

    Units on grid modal.

--- private/js/grid_controller.js
+++ private/js/grid_controller.js
@@ -5,7 +5,7 @@ function GridController($scope) {
             return ($scope.autofill) ? 'ON' : 'OFF';
         },
         toggle: function (e) {
-            $(e.target).toggleClass('btn-success');
+            $(e.target).toggleClass('btn-info');
             $scope.autofill = !$scope.autofill;
         }
     });
